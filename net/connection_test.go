cd67a25178247767597f762651771ac76f4bb7f6
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-05-03 17:53:08 +1200

    Removed unused test

--- net/connection_test.go
+++ net/connection_test.go
@@ -37,35 +37,3 @@ func (s *TestSuite) TestDeserialization(c *C) {
 
 	c.Assert(nextCmd, Equals, cmd)
 }
-
-func (s *TestSuite) TestDeserializationBadPkt(c *C) {
-
-	buf := &bytes.Buffer{}
-
-	type BadUserCommand struct {
-		Actions  uint32
-		Sequence int
-		Msec     uint32
-	}
-
-	// Add the user command
-	cmd := BadUserCommand{}
-	cmd.Actions |= 1 << state.DEAD
-	cmd.Sequence = 12
-	cmd.Msec = 50
-	// Add the game tick
-	binary.Write(buf, binary.LittleEndian, uint32(5))
-	// Add the sequence number
-	binary.Write(buf, binary.LittleEndian, cmd.Sequence)
-	// Add the msec the command was run for
-	binary.Write(buf, binary.LittleEndian, cmd.Msec)
-	// Send the actions
-	binary.Write(buf, binary.LittleEndian, cmd.Actions)
-
-	cc := &ClientConn{}
-	nextCmd, _ := cc.ReadMessage(buf)
-
-	c.Assert(nextCmd.Actions, Equals, cmd.Actions)
-	c.Assert(nextCmd.Sequence, Equals, cmd.Sequence)
-	c.Assert(nextCmd.Msec, Equals, cmd.Msec)
-}
